e3feb2390f3e9f145a42732a18cd8209b34c1ab6
Suho Kwon <suho.kwon@gmail.com>
2024-07-07 11:09:41 +0900

    Add more examples.

---
+++ DeepLearning/example02.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+# Network and training parameters.
+EPOCHS = 200
+BATCH_SIZE = 128
+VERBOSE = 1
+NB_CLASSES = 10 # number of outputs = number of digits
+N_HIDDEN = 128
+VALIDATION_SPLIT = 0.2 # how much TRAIN is reserved for VALIDATION
+# Loading MNIST dataset.
+# verify
+# You can verify that the split between train and test is 60,000, and 10,000 respectively.
+# Labels have one-hot representation.is automatically applied
+mnist = keras.datasets.mnist
+
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+# X_train is 60000 rows of 28x28 values; we --> reshape it to 60000 x 784.
+RESHAPED = 784
+#
+X_train = X_train.reshape(60000, RESHAPED)
+X_test = X_test.reshape(10000, RESHAPED)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+# Normalize inputs to be within in [0, 1].
+X_train /= 255
+X_test /= 255
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# One-hot representation of the labels.
+Y_train = tf.keras.utils.to_categorical(Y_train, NB_CLASSES)
+Y_test = tf.keras.utils.to_categorical(Y_test, NB_CLASSES)
+
+print(Y_train)
+print(Y_test)
