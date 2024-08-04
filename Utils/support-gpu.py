ac5b8ee04d144866f002e1ee64cfacd79c46d10b
Shkwon <suho.kwon@gmail.com>
2024-08-04 15:16:34 +0900

    Add more examples.

---
+++ Utils/support-gpu.py
@@ -0,0 +1,9 @@
+# tensorflow test메소드를 통한 체크
+import tensorflow as tf
+tf.test.is_built_with_cuda()   
+
+
+# device_lib를 통한 체크
+# 실행결과에 cpu, gpu 둘 다 나와야 정상 
+from tensorflow.python.client import device_lib
+print(device_lib.list_local_devices())
