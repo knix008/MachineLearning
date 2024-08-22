25a375eb405f23544559851e58919b56d68204b3
knix008 <suho.kwon@gmail.com>
2024-08-22 16:27:33 +0900

    add more files.'

---
+++ DeepLearning/Chapter05/ManyToMany01.py
@@ -0,0 +1,43 @@
+import numpy as np
+import os
+import shutil
+import tensorflow as tf
+
+def download_and_read(dataset_dir, num_pairs=None):
+    sent_filename = os.path.join(dataset_dir, "treebank-sents.txt")
+    poss_filename = os.path.join(dataset_dir, "treebank-poss.txt")
+    if not(os.path.exists(sent_filename) and os.path.exists(poss_filename)):
+        import nltk    
+        if not os.path.exists(dataset_dir):
+            os.makedirs(dataset_dir)
+        fsents = open(sent_filename, "w")
+        fposs = open(poss_filename, "w")
+        sentences = nltk.corpus.treebank.tagged_sents()
+        for sent in sentences:
+            fsents.write(" ".join([w for w, p in sent]) + "\n")
+            fposs.write(" ".join([p for w, p in sent]) + "\n")
+        fsents.close()
+        fposs.close()
+    sents, poss = [], []
+    with open(sent_filename, "r") as fsent:
+        for idx, line in enumerate(fsent):
+            sents.append(line.strip())
+            if num_pairs is not None and idx >= num_pairs:
+                break
+    with open(poss_filename, "r") as fposs:
+        for idx, line in enumerate(fposs):
+            poss.append(line.strip())
+            if num_pairs is not None and idx >= num_pairs:
+                break
+    return sents, poss
+
+NUM_PAIRS = None
+EMBEDDING_DIM = 128
+RNN_OUTPUT_DIM = 256
+BATCH_SIZE = 128
+NUM_EPOCHS = 50
+
+# download and read source and target data into data structure
+sents, poss = download_and_read("./datasets", num_pairs=NUM_PAIRS)
+assert(len(sents) == len(poss))
+print("# of records: {:d}".format(len(sents)))
