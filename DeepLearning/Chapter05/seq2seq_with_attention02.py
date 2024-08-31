b26179a01bce9d12670b3b4dc49af7e6a62b62b9
Shkwon <suho.kwon@gmail.com>
2024-08-31 15:44:52 +0900

    Add more examples.

--- DeepLearning/Chapter05/seq2seq_with_attention02.py
+++ DeepLearning/Chapter05/seq2seq_with_attention02.py
@@ -8,6 +8,7 @@ import shutil
 import tensorflow as tf
 import os
 import unicodedata
+import zipfile
 
 from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
 
@@ -27,12 +28,13 @@ def preprocess_sentence(sent):
     sent = sent.lower()
     return sent
 
-def download_and_read():
+def download_and_read(url, num_sent_pairs=30000):
+    local_file = url.split('/')[-1]
+    local_file = os.path.join(".", "fra.txt")
     en_sents, fr_sents_in, fr_sents_out = [], [], []
-    local_file = os.path.join("datasets", "fra.txt")
     with open(local_file, "r", encoding="UTF8") as fin:
         for i, line in enumerate(fin):
-            en_sent, fr_sent, _ = line.strip().split('\t')
+            en_sent, fr_sent, _= line.strip().split('\t')
             en_sent = [w for w in preprocess_sentence(en_sent).split()]
             fr_sent = preprocess_sentence(fr_sent)
             fr_sent_in = [w for w in ("BOS " + fr_sent).split()]
@@ -40,20 +42,66 @@ def download_and_read():
             en_sents.append(en_sent)
             fr_sents_in.append(fr_sent_in)
             fr_sents_out.append(fr_sent_out)
-            if i >= NUM_SENT_PAIRS - 1:
+            if i >= num_sent_pairs - 1:
                 break
     return en_sents, fr_sents_in, fr_sents_out
 
+class BahdanauAttention(tf.keras.layers.Layer):
+    def __init__(self, num_units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(num_units)
+        self.W2 = tf.keras.layers.Dense(num_units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, query, values):
+        # query is the decoder state at time step j
+        # query.shape: (batch_size, num_units)
+        # values are encoder states at every timestep i
+        # values.shape: (batch_size, num_timesteps, num_units)
+
+        # add time axis to query: (batch_size, 1, num_units)
+        query_with_time_axis = tf.expand_dims(query, axis=1)
+        # compute score:
+        score = self.V(tf.keras.activations.tanh(
+            self.W1(values) + self.W2(query_with_time_axis)))
+        # compute softmax
+        alignment = tf.nn.softmax(score, axis=1)
+        # compute attended output
+        context = tf.reduce_sum(
+            tf.linalg.matmul(
+                tf.linalg.matrix_transpose(alignment),
+                values
+            ), axis=1
+        )
+        context = tf.expand_dims(context, axis=1)
+        return context, alignment
+
+class LuongAttention(tf.keras.layers.Layer):
+    def __init__(self, num_units):
+        super(LuongAttention, self).__init__()
+        self.W = tf.keras.layers.Dense(num_units)
+
+    def call(self, query, values):
+        # add time axis to query
+        query_with_time_axis = tf.expand_dims(query, axis=1)
+        # compute score
+        score = tf.linalg.matmul(
+            query_with_time_axis, self.W(values), transpose_b=True)
+        # compute softmax
+        alignment = tf.nn.softmax(score, axis=2)
+        # compute attended output
+        context = tf.matmul(alignment, values)
+        return context, alignment
+
 class Encoder(tf.keras.Model):
-    def __init__(self, vocab_size, embedding_dim, num_timesteps, 
-            encoder_dim, **kwargs):
+    def __init__(self, vocab_size, num_timesteps, 
+            embedding_dim, encoder_dim, **kwargs):
         super(Encoder, self).__init__(**kwargs)
         self.encoder_dim = encoder_dim
         self.embedding = tf.keras.layers.Embedding(
-            #vocab_size, embedding_dim, input_length=num_timesteps)
-            vocab_size, embedding_dim)
+            vocab_size, embedding_dim, input_length=num_timesteps)
         self.rnn = tf.keras.layers.GRU(
-            encoder_dim, return_sequences=False, return_state=True)
+            encoder_dim, return_sequences=True, return_state=True)
 
     def call(self, x, state):
         x = self.embedding(x)
@@ -63,25 +111,35 @@ class Encoder(tf.keras.Model):
     def init_state(self, batch_size):
         return tf.zeros((batch_size, self.encoder_dim))
 
-
 class Decoder(tf.keras.Model):
     def __init__(self, vocab_size, embedding_dim, num_timesteps,
             decoder_dim, **kwargs):
         super(Decoder, self).__init__(**kwargs)
         self.decoder_dim = decoder_dim
+
+        # self.attention = LuongAttention(embedding_dim)
+        self.attention = BahdanauAttention(embedding_dim)
+
         self.embedding = tf.keras.layers.Embedding(
-#            vocab_size, embedding_dim, input_length=num_timesteps)
-             vocab_size, embedding_dim)
+            vocab_size, embedding_dim, input_length=num_timesteps)
         self.rnn = tf.keras.layers.GRU(
             decoder_dim, return_sequences=True, return_state=True)
-        self.dense = tf.keras.layers.Dense(vocab_size)
 
-    def call(self, x, state):
+        self.Wc = tf.keras.layers.Dense(decoder_dim, activation="tanh")
+        self.Ws = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, x, state, encoder_out):
         x = self.embedding(x)
+        context, alignment = self.attention(x, encoder_out)
+        x = tf.expand_dims(
+                tf.concat([
+                    x, tf.squeeze(context, axis=1)
+                ], axis=1), 
+            axis=1)
         x, state = self.rnn(x, state)
-        x = self.dense(x)
-        return x, state
-
+        x = self.Wc(x)
+        x = self.Ws(x)
+        return x, state, alignment
 
 def loss_fn(ytrue, ypred):
     scce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
@@ -90,19 +148,23 @@ def loss_fn(ytrue, ypred):
     loss = scce(ytrue, ypred, sample_weight=mask)
     return loss
 
-
 @tf.function
 def train_step(encoder_in, decoder_in, decoder_out, encoder_state):
     with tf.GradientTape() as tape:
         encoder_out, encoder_state = encoder(encoder_in, encoder_state)
         decoder_state = encoder_state
-        decoder_pred, decoder_state = decoder(decoder_in, decoder_state)
-        loss = loss_fn(decoder_out, decoder_pred)
-    
+
+        loss = 0
+        for t in range(decoder_out.shape[1]):
+            decoder_in_t = decoder_in[:, t]
+            decoder_pred_t, decoder_state, _ = decoder(decoder_in_t,
+                decoder_state, encoder_out)
+            loss += loss_fn(decoder_out[:, t], decoder_pred_t)
+
     variables = encoder.trainable_variables + decoder.trainable_variables
     gradients = tape.gradient(loss, variables)
     optimizer.apply_gradients(zip(gradients, variables))
-    return loss
+    return loss / decoder_out.shape[1]
 
 
 def predict(encoder, decoder, batch_size, 
@@ -119,63 +181,99 @@ def predict(encoder, decoder, batch_size,
     encoder_out, encoder_state = encoder(encoder_in, encoder_state)
     decoder_state = encoder_state
 
-    decoder_in = tf.expand_dims(
-        tf.constant([word2idx_fr["BOS"]]), axis=0)
     pred_sent_fr = []
+    decoder_in = tf.expand_dims(
+        tf.constant(word2idx_fr["BOS"]), axis=0)
+
     while True:
-        decoder_pred, decoder_state = decoder(decoder_in, decoder_state)
+        decoder_pred, decoder_state, _ = decoder(
+            decoder_in, decoder_state, encoder_out)
         decoder_pred = tf.argmax(decoder_pred, axis=-1)
         pred_word = idx2word_fr[decoder_pred.numpy()[0][0]]
         pred_sent_fr.append(pred_word)
         if pred_word == "EOS":
             break
-        decoder_in = decoder_pred
-    
-    print("predicted: ", " ".join(pred_sent_fr))
+        decoder_in = tf.squeeze(decoder_pred, axis=1)
 
+    print("predicted: ", " ".join(pred_sent_fr))
 
 def evaluate_bleu_score(encoder, decoder, test_dataset, 
         word2idx_fr, idx2word_fr):
 
     bleu_scores = []
     smooth_fn = SmoothingFunction()
+
     for encoder_in, decoder_in, decoder_out in test_dataset:
         encoder_state = encoder.init_state(batch_size)
         encoder_out, encoder_state = encoder(encoder_in, encoder_state)
         decoder_state = encoder_state
-        decoder_pred, decoder_state = decoder(decoder_in, decoder_state)
 
-        # compute argmax
-        decoder_out = decoder_out.numpy()
-        decoder_pred = tf.argmax(decoder_pred, axis=-1).numpy()
-
-        for i in range(decoder_out.shape[0]):
-            ref_sent = [idx2word_fr[j] for j in decoder_out[i].tolist() if j > 0]
-            hyp_sent = [idx2word_fr[j] for j in decoder_pred[i].tolist() if j > 0]
+        ref_sent_ids = np.zeros_like(decoder_out)
+        hyp_sent_ids = np.zeros_like(decoder_out)
+        for t in range(decoder_out.shape[1]):
+            decoder_out_t = decoder_out[:, t]
+            decoder_in_t = decoder_in[:, t]
+            decoder_pred_t, decoder_state, _ = decoder(
+                decoder_in_t, decoder_state, encoder_out)
+            decoder_pred_t = tf.argmax(decoder_pred_t, axis=-1)
+            for b in range(decoder_pred_t.shape[0]):
+                ref_sent_ids[b, t] = decoder_out_t.numpy()[0]
+                hyp_sent_ids[b, t] = decoder_pred_t.numpy()[0][0]
+        
+        for b in range(ref_sent_ids.shape[0]):
+            ref_sent = [idx2word_fr[i] for i in ref_sent_ids[b] if i > 0]
+            hyp_sent = [idx2word_fr[i] for i in hyp_sent_ids[b] if i > 0]
             # remove trailing EOS
             ref_sent = ref_sent[0:-1]
             hyp_sent = hyp_sent[0:-1]
-            bleu_score = sentence_bleu([ref_sent], hyp_sent, 
+            bleu_score = sentence_bleu([ref_sent], hyp_sent,
                 smoothing_function=smooth_fn.method1)
             bleu_scores.append(bleu_score)
 
     return np.mean(np.array(bleu_scores))
 
+# NUM_SENT_PAIRS = 100
+# EMBEDDING_DIM = 32
+# ENCODER_DIM, DECODER_DIM = 64, 64
+# BATCH_SIZE = 8
+# NUM_EPOCHS = 3
 
 NUM_SENT_PAIRS = 30000
 EMBEDDING_DIM = 256
 ENCODER_DIM, DECODER_DIM = 1024, 1024
 BATCH_SIZE = 64
-NUM_EPOCHS = 50 #30
+NUM_EPOCHS = 30
 
 tf.random.set_seed(42)
 
 data_dir = "./data"
 checkpoint_dir = clean_up_logs(data_dir)
 
+# Test code for attention classes
+# batch_size = BATCH_SIZE
+# num_timesteps = MAXLEN_EN
+# num_units = ENCODER_DIM
+
+# query = np.random.random(size=(batch_size, num_units))
+# values = np.random.random(size=(batch_size, num_timesteps, num_units))
+
+# # check out dimensions for Bahdanau attention
+# b_attn = BahdanauAttention(num_units)
+# context, alignments = b_attn(query, values)
+# print("Bahdanau: context.shape:", context.shape, "alignments.shape:", alignments.shape)
+# # Bahdanau: context.shape: (64, 1024) alignments.shape: (64, 8, 1)
+
+# # check out dimensions for Luong attention
+# l_attn = LuongAttention(num_units)
+# context, alignments = l_attn(query, values)
+# print("Luong: context.shape:", context.shape, "alignments.shape:", alignments.shape)
+# # Luong: context.shape: (64, 1024) alignments.shape: (64, 8, 1)
+# End test code for attention classes
+
 # data preparation
 download_url = "http://www.manythings.org/anki/fra-eng.zip"
-sents_en, sents_fr_in, sents_fr_out = download_and_read()
+sents_en, sents_fr_in, sents_fr_out = download_and_read(
+    download_url, num_sent_pairs=NUM_SENT_PAIRS)
 
 tokenizer_en = tf.keras.preprocessing.text.Tokenizer(
     filters="", lower=False)
@@ -192,15 +290,6 @@ data_fr_in = tf.keras.preprocessing.sequence.pad_sequences(data_fr_in, padding="
 data_fr_out = tokenizer_fr.texts_to_sequences(sents_fr_out)
 data_fr_out = tf.keras.preprocessing.sequence.pad_sequences(data_fr_out, padding="post")
 
-vocab_size_en = len(tokenizer_en.word_index)
-vocab_size_fr = len(tokenizer_fr.word_index)
-word2idx_en = tokenizer_en.word_index
-idx2word_en = {v:k for k, v in word2idx_en.items()}
-word2idx_fr = tokenizer_fr.word_index
-idx2word_fr = {v:k for k, v in word2idx_fr.items()}
-print("vocab size (en): {:d}, vocab size (fr): {:d}".format(
-    vocab_size_en, vocab_size_fr))
-
 maxlen_en = data_en.shape[1]
 maxlen_fr = data_fr_out.shape[1]
 print("seqlen (en): {:d}, (fr): {:d}".format(maxlen_en, maxlen_fr))
@@ -212,6 +301,16 @@ test_size = NUM_SENT_PAIRS // 4
 test_dataset = dataset.take(test_size).batch(batch_size, drop_remainder=True)
 train_dataset = dataset.skip(test_size).batch(batch_size, drop_remainder=True)
 
+vocab_size_en = len(tokenizer_en.word_index)
+vocab_size_fr = len(tokenizer_fr.word_index)
+word2idx_en = tokenizer_en.word_index
+idx2word_en = {v:k for k, v in word2idx_en.items()}
+word2idx_fr = tokenizer_fr.word_index
+idx2word_fr = {v:k for k, v in word2idx_fr.items()}
+print("vocab size (en): {:d}, vocab size (fr): {:d}".format(
+    vocab_size_en, vocab_size_fr))
+# vocab size (en): 57, vocab size (fr): 123
+
 # check encoder/decoder dimensions
 embedding_dim = EMBEDDING_DIM
 encoder_dim, decoder_dim = ENCODER_DIM, DECODER_DIM
@@ -219,20 +318,36 @@ encoder_dim, decoder_dim = ENCODER_DIM, DECODER_DIM
 encoder = Encoder(vocab_size_en+1, embedding_dim, maxlen_en, encoder_dim)
 decoder = Decoder(vocab_size_fr+1, embedding_dim, maxlen_fr, decoder_dim)
 
+# # Test code for encoder and decoder with attention
 # for encoder_in, decoder_in, decoder_out in train_dataset:
+#     print("inputs:", encoder_in.shape, decoder_in.shape, decoder_out.shape)
 #     encoder_state = encoder.init_state(batch_size)
 #     encoder_out, encoder_state = encoder(encoder_in, encoder_state)
 #     decoder_state = encoder_state
-#     decoder_pred, decoder_state = decoder(decoder_in, decoder_state)
+#     decoder_pred = []
+#     for t in range(decoder_out.shape[1]):
+#         decoder_in_t = decoder_in[:, t]
+#         decoder_pred_t, decoder_state, _ = decoder(decoder_in_t,
+#             decoder_state, encoder_out)
+#         decoder_pred.append(decoder_pred_t.numpy())
+#     decoder_pred = tf.squeeze(np.array(decoder_pred), axis=2)
 #     break
 # print("encoder input          :", encoder_in.shape)
 # print("encoder output         :", encoder_out.shape, "state:", encoder_state.shape)
 # print("decoder output (logits):", decoder_pred.shape, "state:", decoder_state.shape)
 # print("decoder output (labels):", decoder_out.shape)
-# # encoder input          : (64, 8)
-# # encoder output         : (64, 1024) state: (64, 1024)
-# # decoder output (logits): (64, 16, 7658) state: (64, 1024)
-# # decoder output (labels): (64, 16)
+
+# Bahdanau:
+# encoder input          : (64, 8)
+# encoder output         : (64, 8, 1024) state: (64, 1024)
+# decoder output (logits): (8, 64, 7658) state: (64, 1024)
+# decoder output (labels): (64, 16)
+# 
+# Luong:
+# encoder input          : (64, 8)
+# encoder output         : (64, 8, 1024) state: (64, 1024)
+# decoder output (logits): (8, 64, 7658) state: (64, 1024)
+# decoder output (labels): (64, 16)
 
 optimizer = tf.keras.optimizers.Adam()
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
