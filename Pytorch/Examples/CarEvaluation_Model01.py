bf2d2413b911b326429dbff9e3f5bb00e517e15c
Shkwon <suho.kwon@gmail.com>
2024-09-18 16:49:45 +0900

    Add more examples.

--- Pytorch/Examples/CarEvaluation_Model01.py
+++ Pytorch/Examples/CarEvaluation_Model01.py
@@ -7,21 +7,20 @@ from sklearn.datasets import load_iris
 import numpy as np
 from sklearn import preprocessing
 import matplotlib.pyplot as plt #importing graph plotting functionality
-# import os
-# print(os.listdir("../input"))
-# Load dataset
-df = pd.read_csv("Datasets/car_evaluation.csv", names = ["buying","maint", "doors", "persons", "lug_boot","safety","class"])
+
+df = pd.read_csv("car_evaluation.csv", names = ["buying","maint", "doors", "persons", "lug_boot","safety","class"])
 
 ## get_dummies() implementation
 category_col =["buying","maint", "doors", "persons", "lug_boot","safety","class"] 
 df = pd.get_dummies(df, columns=category_col)
-df.to_csv('Datasets/car_evaluation_preprocessed.csv',index=False)
+df.to_csv('car_evaluation_preprocessed.csv',index=False)
 ## visualizing processed dataset
 print(df.shape)
-df.head(10)
+print(df.head(10))
 
 X = df.iloc[:, 0:21].values
 y = df.iloc[:, 21:].values
+
 ## Normalizing data - Normalization refers to rescaling real valued numeric attributes into the range 0 and 1.
 X = preprocessing.scale(X)
 from sklearn.model_selection import train_test_split
@@ -33,6 +32,9 @@ labels_train_v = Variable(torch.FloatTensor(labels_train), requires_grad = False
 feature_test_v = Variable(torch.FloatTensor(feature_test), requires_grad = False)
 labels_test_v = Variable(torch.FloatTensor(labels_test), requires_grad = False)
 
+print(feature_train_v.shape)
+print(labels_train_v.shape)
+
 class LinearClassifier(nn.Module):
     def __init__(self):
         super(LinearClassifier, self).__init__()
@@ -42,6 +44,7 @@ class LinearClassifier(nn.Module):
         y = self.h_layer(x)
         p = self.s_layer(y)
         return p
+    
 #declaring the classifier to an object
 model = LinearClassifier()   
 #calculates the loss
@@ -51,6 +54,7 @@ optim = torch.optim.SGD(model.parameters(), lr = 0.01)
 all_losses = []
 for num in range(5000): 
     pred = model(feature_train_v) #predict
+    print(pred.shape)
     loss = loss_fn(pred, labels_train_v) #calculate loss
     all_losses.append(loss.data)
     optim.zero_grad() #zero gradients to not accumulate
@@ -68,12 +72,12 @@ print(all_losses[-1])
 
 
 from sklearn.metrics import accuracy_score
+
 predicted_values = []
 for num in range(len(feature_test_v)):
     predicted_values.append(model(feature_test_v[num]))
 
-    
-    score = 0
+score = 0    
 for num in range(len(predicted_values)):
     if np.argmax(labels_test[num]) == np.argmax(predicted_values[num].data.numpy()):
         score = score + 1
