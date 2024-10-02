530df0456c21564a9cb0977bf98ec95062bae908
Shkwon <suho.kwon@gmail.com>
2024-10-02 13:49:58 +0900

    Add more example.

--- Pytorch/Mastering_Pytorch/Chapter02/LeNet01.py
+++ Pytorch/Mastering_Pytorch/Chapter02/LeNet01.py
@@ -100,13 +100,12 @@ train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=True)
-
-
 test_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=test_transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=10000, shuffle=False)
 
+print(trainset)
+print(testset)
 
 # ordering is important
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
