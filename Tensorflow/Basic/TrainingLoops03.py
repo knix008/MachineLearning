692b22ef12c76315e2cde3ba0a0ed01d9f6ac710
knix008 <suho.kwon@gmail.com>
2024-10-04 14:48:54 +0900

    Add more examples.

---
+++ Tensorflow/Basic/TrainingLoops03.py
@@ -0,0 +1,59 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import warnings 
+warnings.filterwarnings(action='ignore')
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+print("TensorFlow Version : ", tf.__version__)
+
+# The actual line
+TRUE_W = 3.0
+TRUE_B = 2.0
+NUM_EXAMPLES = 201
+
+# A vector of random x values
+x = tf.linspace(-2,2, NUM_EXAMPLES)
+x = tf.cast(x, tf.float32)
+
+def f(x):
+  return x * TRUE_W + TRUE_B
+
+# Generate some noise
+noise = tf.random.normal(shape=[NUM_EXAMPLES])
+
+# Calculate y
+y = f(x) + noise
+#print(y)
+
+class MyModelKeras(tf.keras.Model):
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    # Initialize the weights to `5.0` and the bias to `0.0`
+    # In practice, these should be randomly initialized
+    self.w = tf.Variable(5.0)
+    self.b = tf.Variable(0.0)
+
+  def call(self, x):
+    return self.w * x + self.b
+
+keras_model = MyModelKeras()
+keras_model.summary()
+
+# compile sets the training parameters
+keras_model.compile(
+    # By default, fit() uses tf.function().  You can
+    # turn that off for debugging, but it is on now.
+    run_eagerly=False,
+
+    # Using a built-in optimizer, configuring as an object
+    optimizer=tf.keras.optimizers.SGD(learning_rate=0.1),
+
+    # Keras comes with built-in MSE error
+    # However, you could use the loss function
+    # defined above
+    loss=tf.keras.losses.MeanSquaredError,
+)
+
+print(x.shape[0])
+keras_model.fit(x, y, epochs=10, batch_size=32)
