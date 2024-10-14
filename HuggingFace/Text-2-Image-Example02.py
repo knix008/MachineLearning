9f97b3675258d401ea71c816ff15a04ab63351e9
Shkwon <suho.kwon@gmail.com>
2024-10-14 16:56:59 +0900

    Add more examples.

--- HuggingFace/Text-2-Image-Example02.py
+++ HuggingFace/Text-2-Image-Example02.py
@@ -20,7 +20,7 @@ pipe.to(device)
 
 # if using torch < 2.0
 # pipe.enable_xformers_memory_efficient_attention()
-prompt = " a dog is sitting on a bench in the yard"
+prompt = "a dog is sitting on a bench in the yard"
 image = pipe(prompt=prompt).images[0]
 print("Saving... : ", f"{prompt}.png")
 image.save(f"{prompt}.png")
