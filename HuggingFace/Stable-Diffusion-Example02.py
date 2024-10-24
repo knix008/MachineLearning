72a57b1659b1a64ac34d58e41c0f9b9e04496c81
Shkwon <suho.kwon@gmail.com>
2024-10-24 15:21:17 +0900

    Add more changes.

--- HuggingFace/Stable-Diffusion-Example02.py
+++ HuggingFace/Stable-Diffusion-Example02.py
@@ -6,11 +6,14 @@ os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = 'True'
 import warnings
 warnings.filterwarnings("ignore")
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print("Using... : ", device)
+def main(prompt):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Using... : ", device)
 
-pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16)
-pipeline.to(device)
-prompt = "An image of a squirrel in Picasso style"
-img = pipeline(prompt).images[0]
-img.save(f"{prompt}.jpg")
+    pipeline = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16)
+    pipeline.to(device)
+    img = pipeline(prompt).images[0]
+    img.save(f"{prompt}.jpg")
+    
+if __name__ == "__main__":
+    main("An image of a squirrel in Picasso style")
