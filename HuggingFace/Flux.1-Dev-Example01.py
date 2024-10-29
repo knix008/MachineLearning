176312be25517c05a3e92a12401ca1298bf08875
Suho Kwon <suho.kwon@gmail.com>
2024-10-29 09:08:43 +0900

    Add more examples.

--- HuggingFace/Flux.1-Dev-Example01.py
+++ HuggingFace/Flux.1-Dev-Example01.py
@@ -2,18 +2,20 @@ import os
 import warnings
 import torch
 from diffusers import FluxPipeline
+
 # Disable Hugging Face Warning Messages.
 os.environ['HF_HUB_DISABLE_SYMLINKS_WARNING'] = 'True'
 # Disable all other warning message.
 warnings.filterwarnings("ignore")
+access_token="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"
 
 def run_flux1_dev():
     # Device configuration
-    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #print("Using... : ", device)
-    pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
-    pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
-    #pipe.to(device)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("Using... : ", device)
+    pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16, token=access_token)
+    #pipe.enable_model_cpu_offload() #save some VRAM by offloading the model to CPU. Remove this if you have enough GPU power
+    pipe.to(device)
 
     prompt = "A cat holding a sign that says hello world"
     image = pipe(
