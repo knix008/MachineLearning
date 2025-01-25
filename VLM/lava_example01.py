51883da655ecc0c046e02b11f1485161394e346d
Shkwon <suho.kwon@gmail.com>
2025-01-25 14:38:04 +0900

    Change VLM example

--- VLM/lava_example01.py
+++ VLM/lava_example01.py
@@ -2,6 +2,8 @@ from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 from PIL import Image
 import requests
 import torch
+import time
+import datetime
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
@@ -12,11 +14,21 @@ model = LlavaNextForConditionalGeneration.from_pretrained(
 )
 model.to(device)
 
-url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
-image = Image.open(requests.get(url, stream=True).raw)
+#url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
+file = "Certificate01.png"
+#image = Image.open(requests.get(url, stream=True).raw)
+image = Image.open(file)
 prompt = "[INST] <image>\nWhat is shown in this image? [/INST]"
-
 inputs = processor(prompt, image, return_tensors="pt").to(device)
-output = model.generate(**inputs, max_new_tokens=100)
 
+start = time.time()
+output = model.generate(**inputs, max_new_tokens=1000)
 print(processor.decode(output[0], skip_special_tokens=True))
+end = time.time()
+
+sec = (end - start)
+result = datetime.timedelta(seconds=sec)
+print(result)
+
+result_list = str(datetime.timedelta(seconds=sec)).split(".")
+print(result_list[0])
