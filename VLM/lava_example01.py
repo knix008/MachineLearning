1af684efc8b6901509c4fee449bd8d8c7c2bb791
Shkwon <suho.kwon@gmail.com>
2025-01-25 14:45:13 +0900

    Change VLM example

--- VLM/lava_example01.py
+++ VLM/lava_example01.py
@@ -28,7 +28,5 @@ end = time.time()
 
 sec = (end - start)
 result = datetime.timedelta(seconds=sec)
-print(result)
-
 result_list = str(datetime.timedelta(seconds=sec)).split(".")
 print(result_list[0])
