52006adf498430314dc9892995f0df187147b6d4
Suho Kwon <suho.kwon@gmail.com>
2025-03-24 16:41:56 +0900

    Add more examples.

---
+++ YOLO/yolotutorial02.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+
+def predict():
+    model = YOLO("yolo11n.yaml")
+    model = YOLO("yolo11n.pt")
+    results = model("http://ultralytics.com/images/bus.jpg")
+    print(results)
+    return results
+
+def run_yolo():
+    # Load a model
+    model = YOLO("yolo11n.yaml")
+    model = YOLO("yolo11n.pt")
+
+    results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+    results = model.val()
+    return results
+
+if __name__ == "__main__" :
+    #run_yolo()
+    predict()
