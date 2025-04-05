e796974c544ad757bdfc8fea8b6a3f14eb68db20
Shkwon <suho.kwon@gmail.com>
2025-04-05 14:20:01 +0900

    Add more example.

---
+++ YOLO/yolopose02.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+def train():
+    # Load a model
+    model = YOLO("yolo11n-pose.yaml")  # build a new model from YAML
+    model = YOLO("yolo11n-pose.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolo11n-pose.yaml").load("yolo11n-pose.pt")  # build from YAML and transfer weights
+
+    # Train the model
+    results = model.train(data="coco8-pose.yaml", epochs=100, imgsz=640)
+    print(results)
+
+def validate():
+    # Load a model
+    model = YOLO("yolo11n-pose.pt")  # load an official model
+    model = YOLO("runs/pose/train/weights/best.pt")  # load a custom model
+
+    # Validate the model
+    metrics = model.val()  # no arguments needed, dataset and settings remembered
+    metrics.box.map  # map50-95
+    metrics.box.map50  # map50
+    metrics.box.map75  # map75
+    metrics.box.maps  # a list contains map50-95 of each category
+    
+if __name__ == "__main__":
+    #train()
+    validate()
