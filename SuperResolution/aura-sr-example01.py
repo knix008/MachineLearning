4665a16ce28675722a1f116ac97135eb113e461b
Shkwon <suho.kwon@gmail.com>
2025-04-06 13:38:55 +0900

    Add aura-sr examples.

---
+++ SuperResolution/aura-sr-example01.py
@@ -0,0 +1,17 @@
+from aura_sr import AuraSR
+
+aura_sr = AuraSR.from_pretrained()
+
+import requests
+from io import BytesIO
+from PIL import Image
+
+def load_image_from_url(url):
+    response = requests.get(url)
+    image_data = BytesIO(response.content)
+    return Image.open(image_data)
+
+image = load_image_from_url("https://mingukkang.github.io/GigaGAN/static/images/iguana_output.jpg").resize((256, 256))
+image.save("resized.jpg")
+upscaled_image = aura_sr.upscale_4x(image)
+upscaled_image.save("upscaled.jpg")
