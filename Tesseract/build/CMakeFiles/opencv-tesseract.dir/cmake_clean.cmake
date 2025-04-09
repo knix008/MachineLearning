e26be5b5f3221b1b126eb02c0c69a408145a89d4
Suho Kwon <shkwon@Ubuntu24.04-01>
2025-04-10 08:55:56 +0900

    Add more examples.

---
+++ Tesseract/build/CMakeFiles/opencv-tesseract.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opencv-tesseract.dir/opencv-tesseract-ocr-example01.cpp.o"
+  "CMakeFiles/opencv-tesseract.dir/opencv-tesseract-ocr-example01.cpp.o.d"
+  "opencv-tesseract"
+  "opencv-tesseract.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opencv-tesseract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
