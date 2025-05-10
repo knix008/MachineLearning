26c86efe9b05f7e4384b210a1cc1c2c7e2e34d69
Shkwon <suho.kwon@gmail.com>
2025-05-10 17:49:37 +0900

    ADd more examples.

--- Transformer/huggingface09.py
+++ Transformer/huggingface09.py
@@ -13,3 +13,8 @@ tokenized_datasets = dataset.map(tokenize_function, batched=True)
 small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
 small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
 
+from transformers import DefaultDataCollator
+data_collator = DefaultDataCollator(return_tensors="tf")
+# convert the tokenized datasets to TensorFlow datasets
+tf_train_dataset = small_train_dataset.to_tf_dataset(
+    columns=["attention_mask", "input_ids", "token_type_ids"], label_cols=["labels"], shuffle=True, collate_fn=data_collator, batch_size=8,)
