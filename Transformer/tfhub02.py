4288c0b44fde28e5b1cd6f76fef09e051cb01fd0
Suho Kwon <suho.kwon@gmail.com>
2025-05-15 15:57:58 +0900

    Add more examples.

---
+++ Transformer/tfhub02.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+from sklearn.metrics import pairwise
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text as text # Imports TF ops for preprocessing.
+
+# You need to install some software to run this code.
+# pip install seaborn
+# pip install scikit-learn
+# pip install tensorflow_hub
+# pip install tensorflow_text  <<-- To install tensorflow_text, you need to install python 3.9 version. Edit .env/pyvenv.cfg file and install python 3.9
