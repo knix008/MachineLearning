4517f9515a918108f15cf44163b6426f66d7a931
Suho Kwon <suho.kwon@gmail.com>
2025-05-22 10:43:36 +0900

    Add more examples.

---
+++ Pytorch/Chapter19/Onnx02.py
@@ -0,0 +1,16 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from optimum.onnxruntime import ORTModelForSequenceClassification
+
+# a pre-trained model from HuggingFace
+model_name = "bert-base-uncased"
+onnx_directory = "bert-base-uncased_onnx"
+
+# Load the pre-trained model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", export=True)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+input_ids = tokenizer("I love PyTorch!", return_tensors="pt")
+model(**input_ids)
+
+model_onnx = ORTModelForSequenceClassification.from_pretrained("bert-base-uncased", export=True)
+model_onnx(**input_ids)
