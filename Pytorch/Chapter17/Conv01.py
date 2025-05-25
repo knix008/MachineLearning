ec232f8bf056f113f7438c6ace716d228ee4cb91
Shkwon <suho.kwon@gmail.com>
2025-05-25 16:36:08 +0900

    Add more examples.

--- Pytorch/Chapter17/Conv01.py
+++ Pytorch/Chapter17/Conv01.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
 import os
+
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
 
 import matplotlib.pyplot as plt
@@ -18,6 +19,7 @@ print("> Device : ", device)
 np.random.seed(123)
 torch.use_deterministic_algorithms(True)
 
+
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
@@ -25,9 +27,9 @@ class ConvNet(nn.Module):
         self.cn2 = nn.Conv2d(16, 32, 3, 1)
         self.dp1 = nn.Dropout2d(0.10)
         self.dp2 = nn.Dropout2d(0.25)
-        self.fc1 = nn.Linear(4608, 64) # 4608 is basically 12 X 12 X 32
+        self.fc1 = nn.Linear(4608, 64)  # 4608 is basically 12 X 12 X 32
         self.fc2 = nn.Linear(64, 10)
- 
+
     def forward(self, x):
         x = self.cn1(x)
         x = F.relu(x)
@@ -43,20 +45,28 @@ class ConvNet(nn.Module):
         op = F.log_softmax(x, dim=1)
         return op
 
+
 def train(model, device, train_dataloader, optim, epoch):
     model.train()
     for b_i, (X, y) in enumerate(train_dataloader):
         X, y = X.to(device), y.to(device)
         optim.zero_grad()
         pred_prob = model(X)
-        loss = F.nll_loss(pred_prob, y) # nll is the negative likelihood loss
+        loss = F.nll_loss(pred_prob, y)  # nll is the negative likelihood loss
         loss.backward()
         optim.step()
         if b_i % 10 == 0:
-            print('epoch: {} [{}/{} ({:.0f}%)]\t training loss: {:.6f}'.format(
-                epoch, b_i * len(X), len(train_dataloader.dataset),
-                100. * b_i / len(train_dataloader), loss.item()))
-            
+            print(
+                "epoch: {} [{}/{} ({:.0f}%)]\t training loss: {:.6f}".format(
+                    epoch,
+                    b_i * len(X),
+                    len(train_dataloader.dataset),
+                    100.0 * b_i / len(train_dataloader),
+                    loss.item(),
+                )
+            )
+
+
 def test(model, device, test_dataloader):
     model.eval()
     loss = 0
@@ -65,31 +75,51 @@ def test(model, device, test_dataloader):
         for X, y in test_dataloader:
             X, y = X.to(device), y.to(device)
             pred_prob = model(X)
-            loss += F.nll_loss(pred_prob, y, reduction='sum').item()  # loss summed across the batch
-            pred = pred_prob.argmax(dim=1, keepdim=True)  # us argmax to get the most likely prediction
+            loss += F.nll_loss(
+                pred_prob, y, reduction="sum"
+            ).item()  # loss summed across the batch
+            pred = pred_prob.argmax(
+                dim=1, keepdim=True
+            )  # us argmax to get the most likely prediction
             success += pred.eq(y.view_as(pred)).sum().item()
 
     loss /= len(test_dataloader.dataset)
 
-    print('\nTest dataset: Overall Loss: {:.4f}, Overall Accuracy: {}/{} ({:.0f}%)\n'.format(
-        loss, success, len(test_dataloader.dataset),
-        100. * success / len(test_dataloader.dataset)))
-    
+    print(
+        "\nTest dataset: Overall Loss: {:.4f}, Overall Accuracy: {}/{} ({:.0f}%)\n".format(
+            loss,
+            success,
+            len(test_dataloader.dataset),
+            100.0 * success / len(test_dataloader.dataset),
+        )
+    )
+
+
 # The mean and standard deviation values are calculated as the mean of all pixel values of all images in the training dataset
 train_dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1302,), (0.3069,))])), # train_X.mean()/256. and train_X.std()/256.
-    batch_size=32, shuffle=True)
+    datasets.MNIST(
+        "../data",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1302,), (0.3069,))]
+        ),
+    ),  # train_X.mean()/256. and train_X.std()/256.
+    batch_size=32,
+    shuffle=True,
+)
 
 test_dataloader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, 
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1302,), (0.3069,)) 
-                   ])),
-    batch_size=500, shuffle=True)
+    datasets.MNIST(
+        "../data",
+        train=False,
+        transform=transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize((0.1302,), (0.3069,))]
+        ),
+    ),
+    batch_size=500,
+    shuffle=True,
+)
 
 device = torch.device("cpu")
 
@@ -99,11 +129,11 @@ optimizer = optim.Adadelta(model.parameters(), lr=0.5)
 for epoch in range(1, 20):
     train(model, device, train_dataloader, optimizer, epoch)
     test(model, device, test_dataloader)
-    
+
 test_samples = enumerate(test_dataloader)
 b_i, (sample_data, sample_targets) = next(test_samples)
 
-plt.imshow(sample_data[0][0], cmap='gray', interpolation='none')
+plt.imshow(sample_data[0][0], cmap="gray", interpolation="none")
 plt.show()
 
 print(f"Model prediction is : {model(sample_data).data.max(1)[1][0]}")
@@ -118,32 +148,32 @@ for i in range(len(model_children_list)):
     if type(model_children_list[i]) == nn.Conv2d:
         model_parameters.append(model_children_list[i].weight)
         convolutional_layers.append(model_children_list[i])
-        
+
 plt.figure(figsize=(5, 4))
 for i, flt in enumerate(model_parameters[0]):
-    plt.subplot(4, 4, i+1)
-    plt.imshow(flt[0, :, :].detach(), cmap='gray')
-    plt.axis('off')
+    plt.subplot(4, 4, i + 1)
+    plt.imshow(flt[0, :, :].detach(), cmap="gray")
+    plt.axis("off")
 plt.show()
 
 plt.figure(figsize=(5, 8))
 for i, flt in enumerate(model_parameters[1]):
-    plt.subplot(8, 4, i+1)
-    plt.imshow(flt[0, :, :].detach(), cmap='gray')
-    plt.axis('off')
+    plt.subplot(8, 4, i + 1)
+    plt.imshow(flt[0, :, :].detach(), cmap="gray")
+    plt.axis("off")
 plt.show()
 
 per_layer_results = [convolutional_layers[0](sample_data)]
 for i in range(1, len(convolutional_layers)):
     per_layer_results.append(convolutional_layers[i](per_layer_results[-1]))
-    
+
 plt.figure(figsize=(5, 4))
 layer_visualisation = per_layer_results[0][0, :, :, :]
 layer_visualisation = layer_visualisation.data
 print(layer_visualisation.size())
 for i, flt in enumerate(layer_visualisation):
     plt.subplot(4, 4, i + 1)
-    plt.imshow(flt, cmap='gray')
+    plt.imshow(flt, cmap="gray")
     plt.axis("off")
 plt.show()
 
@@ -153,6 +183,6 @@ layer_visualisation = layer_visualisation.data
 print(layer_visualisation.size())
 for i, flt in enumerate(layer_visualisation):
     plt.subplot(8, 4, i + 1)
-    plt.imshow(flt, cmap='gray')
+    plt.imshow(flt, cmap="gray")
     plt.axis("off")
 plt.show()
