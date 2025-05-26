ec9d3dd0c3d4f801a60350494f8ebb37725aae9e
Suho Kwon <suho.kwon@gmail.com>
2025-05-26 10:52:24 +0900

    Add more examples.

---
+++ Pytorch/Chapter10/Datasets01.py
@@ -0,0 +1,13 @@
+import torch
+import os
+
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+# Checking GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("> Device : ", device)
+
+from datasets import load_dataset
+
+dataset = load_dataset("huggan/selfie2anime", split="train")
+print("> Loading datasets : ", dataset)
+
