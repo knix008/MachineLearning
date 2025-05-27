fe942e46838b7caa730ecd5071cf57419b0a1e3a
Suho Kwon <suho.kwon@gmail.com>
2025-05-27 10:08:23 +0900

    Add more examples.

---
+++ Pytorch/Chapter10/Datasets03.py
@@ -0,0 +1,34 @@
+import torch
+import os
+
+os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
+# Checking GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("> Device : ", device)
+
+from datasets import load_dataset, Image, Dataset
+
+dataset = load_dataset("huggan/selfie2anime", split="train")
+#print(dataset)
+
+from torchvision import transforms
+
+IMAGE_SIZE = 128
+preprocess = transforms.Compose(
+    [
+        transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5]),
+    ]
+)
+
+#img = dataset["imageB"][0]
+#Sprint(img)
+
+def transform(examples):
+    images = [preprocess(image) for image in examples["imageB"]]
+    return {"images": images}
+
+dataset.set_transform(transform)
+#print("> Transformed ", dataset)
