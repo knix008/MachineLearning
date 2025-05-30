ceda2bdaaf5efc0c6cc86268953c0a05ec783b05
knix008 <suho.kwon@gmail.com>
2025-05-30 16:50:15 +0900

    Add more examples.

---
+++ Kaggle/KaggleHubDownload01.py
@@ -0,0 +1,49 @@
+import torch
+import torchvision
+import kagglehub
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+class StanfordCars(torch.utils.data.Dataset):
+    def __init__(self, root_path, transform=None):
+        self.images = [os.path.join(root_path, file) for file in os.listdir(root_path)]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, index):
+        image_file = self.images[index]
+        image = Image.open(image_file).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image[None]
+
+
+def show_images(dataset, num_samples=20, cols=4):
+    """Plots some samples from the dataset"""
+    plt.figure(figsize=(15, 15))
+    for i, img in enumerate(dataset):
+        if i == num_samples:
+            break
+        plt.subplot(int(num_samples / cols) + 1, cols, i + 1)
+        plt.imshow(img[0].permute(1, 2, 0))  # Convert to HWC format for plotting
+        plt.axis("off")
+    plt.show()
+
+
+if __name__ == "__main__":
+    # ​Download latest version
+    image_path = r"vehicle dataset\train\images"
+    
+    path = kagglehub.dataset_download(
+        "nadinpethiyagoda/vehicle-dataset-for-yolo", force_download=False
+    )
+    print("Path to dataset files:", path)
+    train_image_path = os.path.join(path, image_path)
+    #print("Training images path : ", train_image_path)
+    stanfordcars = StanfordCars(
+        train_image_path, transform=torchvision.transforms.ToTensor()
+    )
+    show_images(stanfordcars)
---
+++ Kaggle/PathTest01.py
@@ -0,0 +1,7 @@
+import os
+
+list = os.listdir(".")
+print(list)
+
+list = os.listdir(r"C:\Users\shkwon\.cache\kagglehub\datasets\nadinpethiyagoda\vehicle-dataset-for-yolo\versions\1\vehicle dataset\train\images")
+print(list)
