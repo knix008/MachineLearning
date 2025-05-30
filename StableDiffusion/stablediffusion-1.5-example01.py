b03e11691b705134fadbf8a9f43db0b82a5dc57c
knix008 <suho.kwon@gmail.com>
2025-05-30 13:27:53 +0900

    Add changes.

--- StableDiffusion/stablediffusion-1.5-example01.py
+++ StableDiffusion/stablediffusion-1.5-example01.py
@@ -3,12 +3,15 @@ from diffusers import StableDiffusionPipeline
 import matplotlib.pyplot as plt
 import os
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("> Device : ", device)
+
 # Disable warning messages.
 os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "True"
 
 text2img_pipe = StableDiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16
-).to("cuda:0")
+).to(device)
 
 # generate an image
 prompt = "high resolution, a photograph of an astronaut riding a horse"
