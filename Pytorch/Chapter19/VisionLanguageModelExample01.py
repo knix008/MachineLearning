5b14c0734d009016cfd90c8eede1550ae461b1e1
Suho Kwon <suho.kwon@gmail.com>
2025-06-10 09:51:27 +0900

    Add more examples.

---
+++ Pytorch/Chapter19/VisionLanguageModelExample01.py
@@ -0,0 +1,35 @@
+# This example is from "https://huggingface.co/blog/vlms"
+import torch
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+from PIL import Image
+
+def initialize_device():
+     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+     return device
+    
+def initialize_model(device):
+    processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-mistral-7b-hf")
+    model = LlavaNextForConditionalGeneration.from_pretrained(
+        "llava-hf/llava-v1.6-mistral-7b-hf",
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True
+    )
+    model.to(device)
+    return model, processor
+
+
+def main():
+    sample = "llava_v1_5_radar.jpg"
+    image = Image.open(sample)
+    prompt = "[INST] <image>\nWhat is shown in this image? [/INST]"
+    
+    device = initialize_device()
+    model, processor = initialize_model(device)
+
+    inputs = processor(prompt, image, return_tensors="pt").to(device)
+    output = model.generate(**inputs, max_new_tokens=100)
+
+    print(processor.decode(output[0], skip_special_tokens=True))
+    
+if __name__ == "__main__":
+    main()
