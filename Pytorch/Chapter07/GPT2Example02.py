221a6cdc78d9faa097af863e63b93a0006899bde
Suho Kwon <suho.kwon@gmail.com>
2025-06-12 15:18:31 +0900

    Add more examples

---
+++ Pytorch/Chapter07/GPT2Example02.py
@@ -0,0 +1,27 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import torch
+
+
+def main():
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  torch.manual_seed(799)
+  tkz = GPT2Tokenizer.from_pretrained("gpt2")
+  mdl = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
+  ln = 10
+  cue = "They"
+  gen = tkz(cue, return_tensors="pt")
+  to_ret = gen["input_ids"][0]
+
+  prv = None
+  for i in range(ln):
+      outputs = mdl(**gen)
+      next_token_logits = torch.argmax(outputs.logits[-1, :])
+      to_ret = torch.cat([to_ret, next_token_logits.unsqueeze(0)])
+      gen = {"input_ids": to_ret}
+  seq = tkz.decode(to_ret)
+  return seq
+
+
+if __name__ == "__main__":
+    seq = main()
+    print(seq)
