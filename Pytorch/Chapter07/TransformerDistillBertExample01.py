38daf1af0e9c383c6a795f6ebe3687e786575eb2
Suho Kwon <suho.kwon@gmail.com>
2025-06-12 13:30:37 +0900

    Add more examples.

--- Pytorch/Chapter07/TransformerDistillBertExample01.py
+++ Pytorch/Chapter07/TransformerDistillBertExample01.py
@@ -20,20 +20,7 @@ torch.backends.cudnn.deterministic = True
 RANDOM_SEED = 123
 torch.manual_seed(RANDOM_SEED)
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 NUM_EPOCHS = 3
-
-# url = "https://github.com/rasbt/machine-learning-book/raw/main/ch08/movie_data.csv.gz"
-# filename = url.split("/")[-1]
-filename = "movie_data.csv"
-
-with open(filename, "wb") as f:
-    r = requests.get(url)
-    f.write(r.content)
-
-with gzip.open("movie_data.csv.gz", "rb") as f_in:
-    with open("movie_data.csv", "wb") as f_out:
-        shutil.copyfileobj(f_in, f_out)
 
 df = pd.read_csv("movie_data.csv")
 df.head()
