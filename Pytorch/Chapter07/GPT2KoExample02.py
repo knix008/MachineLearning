e60a0d0757910ea013b971041f5fb9d5190c3d5b
Shkwon <suho.kwon@gmail.com>
2025-06-13 21:05:16 +0900

    Add changes.

--- Pytorch/Chapter07/GPT2KoExample02.py
+++ Pytorch/Chapter07/GPT2KoExample02.py
@@ -2,7 +2,7 @@ from transformers import BertTokenizerFast, GPT2LMHeadModel
 import torch
 
 
-def top_k_sampling():
+def top_p_sampling():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("> Using device : ", device)
     tokenizer = BertTokenizerFast.from_pretrained("kykim/gpt3-kor-small_based_on_gpt2")
@@ -11,22 +11,23 @@ def top_k_sampling():
     tokenizer.add_special_tokens({"pad_token": "[PAD]"})
     input = tokenizer(cue, return_tensors="pt", padding=True, truncation=True).to(device)
     attention_mask = input["attention_mask"]
-    
+
     for i in range(3):
         torch.manual_seed(i + 10)
-        op_top_k = model.generate(
+        output_top_p = model.generate(
             input["input_ids"],
             max_length=10,
+            top_p=0.75,
+            top_k=0,
             pad_token_id=tokenizer.pad_token_id,
             attention_mask=attention_mask,
             do_sample=True,
-            top_k=2,
         )
 
-        seq = tokenizer.decode(op_top_k[0], skip_special_tokens=True)
-        print(f"> Output {i + 1}: {seq}")
+        sequence = tokenizer.decode(output_top_p[0], skip_special_tokens=True)
+        print(f"> Output {i + 1}: {sequence}")
 
 
 if __name__ == "__main__":
-    print("> Top-k Sampling")
-    top_k_sampling()
+    print("> Top-p Sampling")
+    top_p_sampling()
