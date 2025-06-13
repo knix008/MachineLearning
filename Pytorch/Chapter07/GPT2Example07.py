39afb5625e64c1c777bf0a538058c4083a71fde8
Shkwon <suho.kwon@gmail.com>
2025-06-13 20:36:49 +0900

    Add changes.

--- Pytorch/Chapter07/GPT2Example07.py
+++ Pytorch/Chapter07/GPT2Example07.py
@@ -2,10 +2,9 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 
 
-def top_k_sampling():
+def greedy_search():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("> Using device : ", device)
-    torch.manual_seed(799)
     tkz = GPT2Tokenizer.from_pretrained("gpt2")
     mdl = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
     cue = "They"
@@ -14,18 +13,18 @@ def top_k_sampling():
     input = tkz(cue, return_tensors="pt", padding=True, truncation=True).to(device)
     attention_mask = input["attention_mask"]
     for i in range(3):
-        op_top_k = mdl.generate(
+        torch.manual_seed(i+10)
+        op_greedy = mdl.generate(
             input["input_ids"],
             max_length=5,
             pad_token_id=tkz.eos_token_id,
             attention_mask=attention_mask,
-            do_sample=True,
-            top_k=2
+            do_sample=False,  # Greedy search does not sample
         )
-        
-        seq = tkz.decode(op_top_k[0], skip_special_tokens=True)
+        seq = tkz.decode(op_greedy[0], skip_special_tokens=True)
         print(f"> Output {i + 1}: {seq}")
 
+
 if __name__ == "__main__":
-    print("> Top-K Sampling")
-    top_k_sampling()
+    print("> Greedy Search 1")
+    greedy_search()
--- Pytorch/Chapter07/GPT2Example08.py
+++ Pytorch/Chapter07/GPT2Example08.py
@@ -2,10 +2,9 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 import torch
 
 
-def top_k_sampling():
+def top_p_sampling():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("> Using device : ", device)
-    torch.manual_seed(799)
     tkz = GPT2Tokenizer.from_pretrained("gpt2")
     mdl = GPT2LMHeadModel.from_pretrained("gpt2").to(device)
     cue = "They"
@@ -14,18 +13,21 @@ def top_k_sampling():
     input = tkz(cue, return_tensors="pt", padding=True, truncation=True).to(device)
     attention_mask = input["attention_mask"]
     for i in range(3):
+        torch.manual_seed(i + 10)
         op_top_k = mdl.generate(
             input["input_ids"],
             max_length=5,
+            top_p=0.75,
+            top_k=0,
             pad_token_id=tkz.eos_token_id,
             attention_mask=attention_mask,
             do_sample=True,
-            top_k=2
         )
-        
+
         seq = tkz.decode(op_top_k[0], skip_special_tokens=True)
         print(f"> Output {i + 1}: {seq}")
 
+
 if __name__ == "__main__":
-    print("> Top-K Sampling")
-    top_k_sampling()
+    print("> Top-P Sampling")
+    top_p_sampling()
