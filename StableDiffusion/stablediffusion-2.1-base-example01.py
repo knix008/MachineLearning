f01079720fa1d81f421782f1aa071becc370b02b
Suho Kwon <suho.kwon@gmail.com>
2025-06-18 16:16:16 +0900

    Add more examples.

--- StableDiffusion/stablediffusion-2.1-base-example01.py
+++ StableDiffusion/stablediffusion-2.1-base-example01.py
@@ -5,20 +5,11 @@ import time
 
 
 def generate_high_resolution_image(prompt, resolution=(1024, 1024)):
-    model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+    model_id = "stabilityai/stable-diffusion-base-2.1"
     base = DiffusionPipeline.from_pretrained(
         model_id, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
     )
     base.to("cuda")
-    refiner = DiffusionPipeline.from_pretrained(
-        model_id,
-        text_encoder_2=base.text_encoder_2,
-        vae=base.vae,
-        torch_dtype=torch.float16,
-        use_safetensors=True,
-        variant="fp16",
-    )
-    refiner.to("cuda")
 
     n_steps = 40
     high_noise_frac = 0.8
@@ -31,14 +22,7 @@ def generate_high_resolution_image(prompt, resolution=(1024, 1024)):
         height=resolution[1],
         width=resolution[0],
     ).images
-    image = refiner(
-        prompt=prompt,
-        num_inference_steps=n_steps,
-        denoising_start=high_noise_frac,
-        image=image,
-        height=resolution[1],
-        width=resolution[0],
-    ).images[0]
+    image = image[0]
 
     return image
 
