f1caa4f2c6c8e60344899c04128743bab7ef098e
Shkwon <suho.kwon@gmail.com>
2025-06-20 22:45:22 +0900

    Add more examples.

--- Pytorch/Chapter10/StableDiffusion_Upscaling01.py
+++ Pytorch/Chapter10/StableDiffusion_Upscaling01.py
@@ -1,34 +1,23 @@
+import requests
+from PIL import Image
+from io import BytesIO
+from diffusers import StableDiffusionUpscalePipeline
 import torch
-from diffusers import StableDiffusionPipeline, StableDiffusionLatentUpscalePipeline
 
-import warnings
-warnings.filterwarnings("ignore")
+# load model and scheduler
+model_id = "stabilityai/stable-diffusion-x4-upscaler"
+pipeline = StableDiffusionUpscalePipeline.from_pretrained(
+    model_id, torch_dtype=torch.float16
+)
+pipeline = pipeline.to("cuda")
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# let's download an  image
+url = "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd2-upscale/low_res_cat.png"
+response = requests.get(url)
+low_res_img = Image.open(BytesIO(response.content)).convert("RGB")
+low_res_img = low_res_img.resize((128, 128))
 
-# 1. 기본 SD 2.0 모델 로드
-base_model = "stabilityai/stable-diffusion-2-1-base"
-pipe = StableDiffusionPipeline.from_pretrained(
-    base_model, torch_dtype=torch.float16, variant="fp16"
-).to(device)
+prompt = "a white cat"
 
-# 프롬프트 설정
-prompt = "a vivid blue and yellow macaw in a tropical jungle, cinematic lighting, ultra-high detail"
-
-# 중간 해상도 이미지 생성
-low_res_image = pipe(prompt=prompt, height=768, width=1152).images[0]
-
-# 2. 업스케일 모델 로드
-upscale_model = "stabilityai/stable-diffusion-x4-upscaler"
-upscale_pipe = StableDiffusionLatentUpscalePipeline.from_pretrained(
-    upscale_model, torch_dtype=torch.float16
-).to(device)
-
-# 고해상도 업스케일 이미지 생성
-high_res_image = upscale_pipe(
-    prompt=prompt, image=low_res_image, num_inference_steps=50, guidance_scale=7.5
-).images[0]
-
-# 저장
-high_res_image.save("macaw_highres_upscaled.png")
-print("업스케일된 이미지가 저장되었습니다.")
+upscaled_image = pipeline(prompt=prompt, image=low_res_img).images[0]
+upscaled_image.save("upsampled_cat.png")
