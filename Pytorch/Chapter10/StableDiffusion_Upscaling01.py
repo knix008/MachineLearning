e836c222e382eb0e969a1312162b755e5d56d9e2
Shkwon <suho.kwon@gmail.com>
2025-06-21 14:58:23 +0900

    Add more examples.

--- Pytorch/Chapter10/StableDiffusion_Upscaling01.py
+++ Pytorch/Chapter10/StableDiffusion_Upscaling01.py
@@ -10,14 +10,13 @@ pipeline = StableDiffusionUpscalePipeline.from_pretrained(
     model_id, torch_dtype=torch.float16
 )
 pipeline = pipeline.to("cuda")
+IMAGE_PATH = "generated_image.png"
 
-# let's download an  image
-url = "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd2-upscale/low_res_cat.png"
-response = requests.get(url)
-low_res_img = Image.open(BytesIO(response.content)).convert("RGB")
-low_res_img = low_res_img.resize((128, 128))
+low_res_img = Image.open(IMAGE_PATH).convert("RGB")
+low_res_img = low_res_img.resize((512, 512))
+low_res_img.save("low_res_cat.png")
 
 prompt = "a white cat"
 
 upscaled_image = pipeline(prompt=prompt, image=low_res_img).images[0]
-upscaled_image.save("upsampled_cat.png")
+upscaled_image.save("upscaled_image.png")
---
+++ Pytorch/Chapter10/StableDiffusion_Upscaling02.py
@@ -0,0 +1,72 @@
+import torch
+from diffusers import StableDiffusionPipeline, StableDiffusionUpscalePipeline
+import torchvision.transforms as T
+import warnings
+from PIL import Image
+
+warnings.filterwarnings("ignore")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_generater_id = "stabilityai/stable-diffusion-2-1-base"
+model_upscaler_id = "stabilityai/stable-diffusion-x4-upscaler"
+
+
+def initialize_generator_pipeline():
+    # Load base Stable Diffusion 2.1 pipeline
+    generator_pipe = StableDiffusionPipeline.from_pretrained(
+        model_generater_id, torch_dtype=torch.float16, variant="fp16"
+    ).to(device)
+    generator_pipe.enable_model_cpu_offload()
+    return generator_pipe
+
+
+def generate_image(generator_pipe, prompt, seed, image_size=512):
+    # Generate intermediate high-res image (PIL)
+    low_res_image = generator_pipe(
+        prompt=prompt,
+        height=image_size,
+        width=image_size,
+        generator=seed,
+        num_inference_steps=30,
+    ).images[0]
+    return low_res_image
+
+
+def initialize_upscaler_pipeline():
+    # Load the Stable Diffusion Upscale pipeline
+    upscaler_pipe = StableDiffusionUpscalePipeline.from_pretrained(
+        model_upscaler_id,
+        torch_dtype=torch.float16,
+        variant="fp16",
+    ).to(device)
+    upscaler_pipe.enable_model_cpu_offload()
+    return upscaler_pipe
+
+
+def upscale_image(upscaler_pipe, prompt, low_res_image):
+    # Upscale the image
+    upscaled_image = upscaler_pipe(prompt=prompt, image=low_res_image).images[0]
+    return upscaled_image
+
+
+IMAGE_SIZE = 512
+
+
+def main():
+    generator_pipe = initialize_generator_pipeline()
+    prompt = "a vivid blue and yellow macaw in a tropical jungle, cinematic lighting, ultra-high detail"
+    seed = torch.manual_seed(42)
+    image = generate_image(generator_pipe, prompt, seed, image_size=IMAGE_SIZE)
+    image.save("generated_image.png")
+    # Upscaling generated image
+    print("> Upscaling generated image...")
+    image = Image.open("generated_image.png").convert("RGB")
+    image = image.resize((IMAGE_SIZE, IMAGE_SIZE))
+    upscaler_pipe = initialize_upscaler_pipeline()
+    image = upscale_image(upscaler_pipe, prompt, image)
+    image.save("upscaled_image.png")
+
+
+if __name__ == "__main__":
+    print("> Generating and upscaling image...")
+    main()
