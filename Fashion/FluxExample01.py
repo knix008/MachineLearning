d22314d266f54a032d0921e0807b0f43f76112f7
Suho Kwon <suho.kwon@gmail.com>
2025-07-07 12:04:29 +0900

    Add changes.

--- Fashion/FluxExample01.py
+++ Fashion/FluxExample01.py
@@ -4,9 +4,17 @@ import gradio as gr
 import time
 from PIL import Image
 
+access_token = ""
+
+from huggingface_hub import login
+login(access_token)
+
 # 모델 로딩 (최초 한 번만)
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe = FluxPipeline.from_pretrained(
+    "black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16
+)
 pipe.enable_model_cpu_offload()
+
 
 def generate_image(prompt):
     start_time = time.time()
@@ -16,10 +24,11 @@ def generate_image(prompt):
         guidance_scale=0.0,
         num_inference_steps=4,
         max_sequence_length=256,
-        generator=generator
+        generator=generator,
     ).images[0]
     elapsed = time.time() - start_time
     return image, f"걸린 시간: {elapsed:.2f}초"
+
 
 examples = [
     ["A cat holding a sign that says hello world"],
@@ -32,7 +41,7 @@ iface = gr.Interface(
     outputs=[gr.Image(label="생성된 이미지"), gr.Textbox(label="걸린 시간 (초)")],
     title="FLUX Schnell 이미지 생성기",
     examples=examples,
-    flagging_mode="never"
+    flagging_mode="never",
 )
 
 if __name__ == "__main__":
