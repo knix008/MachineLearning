33993529082db94491823f5a20607dc52f3724be
Suho Kwon <suho.kwon@gmail.com>
2025-07-08 15:08:07 +0900

    Add more changes.

--- Pytorch/Utils/Pytorch-check-gpu.py
+++ Pytorch/Utils/Pytorch-check-gpu.py
@@ -4,3 +4,4 @@ import torch
 print("PyTorch version:", torch.__version__)
 print("CUDA available:", torch.cuda.is_available())
 print("Number of GPUs:", torch.cuda.device_count())
+print("Current GPU device:", torch.cuda.current_device())
