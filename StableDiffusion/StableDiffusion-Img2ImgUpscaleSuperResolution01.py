6b460f068895952fbf997dfc5f76dd4c573361a8
Suho Kwon <suho.kwon@gmail.com>
2025-08-03 20:06:16 +0900

    Add changes.

--- StableDiffusion/StableDiffusion-Img2ImgUpscaleSuperResolution01.py
+++ StableDiffusion/StableDiffusion-Img2ImgUpscaleSuperResolution01.py
@@ -3,7 +3,10 @@ from diffusers import AutoPipelineForImage2Image
 from diffusers.utils import make_image_grid, load_image
 
 pipeline = AutoPipelineForImage2Image.from_pretrained(
-    "stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    "stable-diffusion-v1-5/stable-diffusion-v1-5",
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True,
 )
 pipeline.enable_model_cpu_offload()
 # remove following line if xFormers is not installed or you have PyTorch 2.0 or higher installed
@@ -12,11 +15,10 @@ pipeline.enable_xformers_memory_efficient_attention()
 # prepare image
 url = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/img2img-init.png"
 init_image = load_image(url)
-
 prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
-
 # pass prompt and image to pipeline
 image_1 = pipeline(prompt, image=init_image, output_type="latent").images[0]
+image_1.save("astronaut_in_jungle_latent.png")
 
 from diffusers import StableDiffusionLatentUpscalePipeline
 
@@ -25,19 +27,19 @@ upscaler = StableDiffusionLatentUpscalePipeline.from_pretrained(
 )
 upscaler.enable_model_cpu_offload()
 upscaler.enable_xformers_memory_efficient_attention()
-
 image_2 = upscaler(prompt, image=image_1).images[0]
-
+image_2.save("astronaut_in_jungle_upscaled.png")
 
 from diffusers import StableDiffusionUpscalePipeline
 
 super_res = StableDiffusionUpscalePipeline.from_pretrained(
-    "stabilityai/stable-diffusion-x4-upscaler", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    "stabilityai/stable-diffusion-x4-upscaler",
+    torch_dtype=torch.float16,
+    variant="fp16",
+    use_safetensors=True,
 )
 super_res.enable_model_cpu_offload()
 super_res.enable_xformers_memory_efficient_attention()
-
 image_3 = super_res(prompt, image=image_2).images[0]
 make_image_grid([init_image, image_3.resize((512, 512))], rows=1, cols=2)
-
 image_3.save("astronaut_in_jungle_super_res.png")
