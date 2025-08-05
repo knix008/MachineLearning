35e433b8803aabd7f153177312adf07bfcec52e5
Suho Kwon <suho.kwon@gmail.com>
2025-08-06 00:46:00 +0900

    Add changes.

--- Flux1/Flux1-dev-ControlNet-Upscaler05.py
+++ Flux1/Flux1-dev-ControlNet-Upscaler05.py
@@ -105,8 +105,8 @@ with gr.Blocks(title="FLUX.1 ControlNet 업스케일러") as demo:
             )
             steps_slider = gr.Slider(
                 minimum=10,
-                maximum=50,
-                value=28,
+                maximum=100,
+                value=50,
                 step=1,
                 label="추론 스텝 수",
                 info="이미지 생성 단계 수. 높을수록 품질이 향상되지만 생성 시간이 늘어남.",
