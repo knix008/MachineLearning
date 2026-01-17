b2382ba85c04cada129abbcd64d3d73b7a2abb19
Suho Kwon <suho.kwon@gmail.com>
2026-01-17 23:41:39 +0900

    Add changes.

---
+++ Flux2/Flux.2-Dev-Klein-Example03.py
@@ -0,0 +1,26 @@
+import torch
+from diffusers import Flux2KleinPipeline
+from datetime import datetime
+
+device = "cuda"
+dtype = torch.bfloat16
+
+pipe = Flux2KleinPipeline.from_pretrained(
+    "black-forest-labs/FLUX.2-klein-4B", torch_dtype=dtype
+)
+pipe.enable_model_cpu_offload()  # save some VRAM by offloading the model to CPU
+
+prompt = "A cat holding a sign that says hello world"
+image = pipe(
+    prompt=prompt,
+    height=1024,
+    width=1024,
+    guidance_scale=1.0,
+    num_inference_steps=4,
+    generator=torch.Generator(device=device).manual_seed(0),
+).images[0]
+
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+filename = f"flux-klein_{timestamp}.png"
+image.save(filename)
+print(f"이미지가 저장되었습니다: {filename}")
