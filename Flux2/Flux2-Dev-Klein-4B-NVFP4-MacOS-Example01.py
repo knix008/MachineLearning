2c00cb075641a2395a96a2384215c960fc1515cf
Suho Kwon <suho.kwon@gmail.com>
2026-01-23 15:35:10 +0900

    Add changes.

--- Flux2/Flux2-Dev-Klein-4B-NVFP4-MacOS-Example01.py
+++ Flux2/Flux2-Dev-Klein-4B-NVFP4-MacOS-Example01.py
@@ -10,7 +10,7 @@ pipe = Flux2KleinPipeline.from_pretrained("black-forest-labs/FLUX.2-klein-4B", t
 pipe = pipe.to(device)
 #pipe.enable_model_cpu_offload()  # save some VRAM by offloading the model to CPU
 
-prompt = "A highly realistic, 4k, high-quality photo of a beautiful skinny animation style girl on vacation. She has black, medium-length hair that reaches her shoulders, tied back in a casual yet stylish manner. She is walking on a sunny beach and her eyes are hazel, wearing a red bikini. The image should capture her in a full-body shot, with perfect anatomy, including precise details in her eyes and teeth. Her skin should appear natural, avoiding an overly smooth or filtered look, to maintain a lifelike. The overall atmosphere is bright and joyful, reflecting the sunny, relaxed vacation mood."
+prompt = "A highly realistic, 4k, high-quality vivid photo of a beautiful skinny animation-style girl. She has black, medium-length hair that reaches her shoulders, tied back in a casual yet stylish manner. She is walking on a sunny beach and her eyes are hazel, wearing a red bikini. The image should capture her in a full-body shot, with perfect anatomy, including precise details in her eyes and teeth. Her skin should appear natural, avoiding an overly smooth or filtered look, to maintain a lifelike. The overall atmosphere is bright and joyful, reflecting the sunny, relaxed vacation mood."
 
 image = pipe(
     prompt=prompt,
