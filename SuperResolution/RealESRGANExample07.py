76858ed35ff798d4d4c29ed014843435763559ec
Suho Kwon <suho.kwon@gmail.com>
2026-01-30 15:25:44 +0900

    Add changes.

--- SuperResolution/RealESRGANExample07.py
+++ SuperResolution/RealESRGANExample07.py
@@ -110,7 +110,7 @@ with gr.Blocks() as demo:
                 label="입력 이미지 (업스케일할 원본)",
                 type="pil",
                 height=500,
-                value="default.jpg",
+                value="sample.png",
             )
             upscale = gr.Slider(
                 minimum=1,
