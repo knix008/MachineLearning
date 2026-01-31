35a1e9811c1c10c24635d4ee83ffb8acf3d01400
Suho Kwon <suho.kwon@gmail.com>
2026-02-01 00:05:42 +0900

    Add changes.

--- Flux2-Dev/Flux.2-Dev-Klein-Base-9B-Example01-MacOS.py
+++ Flux2-Dev/Flux.2-Dev-Klein-Base-9B-Example01-MacOS.py
@@ -1,38 +1,25 @@
-import torch
-from diffusers import Flux2Pipeline
-from datetime import datetime
 import os
-
-# Pre-shifted custom sigmas for 8-step turbo inference
-TURBO_SIGMAS = [1.0, 0.6509, 0.4374, 0.2932, 0.1893, 0.1108, 0.0495, 0.00031]
+import torch
+from diffusers import Flux2KleinPipeline
+from datetime import datetime
 
 device = "mps"
-device_type = torch.bfloat16
+dtype = torch.bfloat16
 
-pipe = Flux2Pipeline.from_pretrained(
-    "black-forest-labs/FLUX.2-dev", torch_dtype=device_type
-)
-
-pipe.load_lora_weights(
-    "fal/FLUX.2-dev-Turbo", weight_name="flux.2-turbo-lora.safetensors"
-)
-print("LoRA weights loaded.")
+pipe = Flux2KleinPipeline.from_pretrained("black-forest-labs/FLUX.2-klein-base-9B", torch_dtype=dtype)
+#pipe.enable_model_cpu_offload()  # save some VRAM by offloading the model to CPU
 
 prompt = "A highly realistic, high-quality photo of a beautiful Instagram-style cute korean girl on vacation. She has black, medium-length hair that reaches her shoulders, tied back in a casual yet stylish manner. Her eyes are hazel, with a natural sparkle of happiness as she smiles. The image should capture her in a full-body shot, with perfect anatomy, including precise details in her eyes and teeth. Her skin should appear natural, with visible pores, avoiding an overly smooth or filtered look, to maintain a lifelike, 4K resolution quality. The overall atmosphere is bright and joyful, reflecting the sunny, relaxed vacation mood."
 
-
 image = pipe(
-    prompt=prompt,
-    sigmas=TURBO_SIGMAS,
-    guidance_scale=2.5,
+    prompt,
     height=1024,
     width=1024,
-    num_inference_steps=8,
+    guidance_scale=4.0,
+    num_inference_steps=50,
+    generator=torch.Generator(device=device).manual_seed(0)
 ).images[0]
 
-# Generate filename with script name and current date and time
 script_name = os.path.splitext(os.path.basename(__file__))[0]
 timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-output_filename = f"{script_name}_{timestamp}.png"
-image.save(output_filename)
-print(f"이미지가 저장되었습니다: {output_filename}")
+image.save(f"{script_name}_{timestamp}.png")
