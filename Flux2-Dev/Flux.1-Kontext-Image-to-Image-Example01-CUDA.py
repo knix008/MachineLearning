c6357eca063a5ffbbd516616451136287cfc4473
Suho Kwon <suho.kwon@gmail.com>
2026-02-03 12:07:36 +0900

    Add changes.

--- Flux2-Dev/Flux.1-Kontext-Image-to-Image-Example01-CUDA.py
+++ Flux2-Dev/Flux.1-Kontext-Image-to-Image-Example01-CUDA.py
@@ -4,15 +4,25 @@ from diffusers.utils import load_image
 from datetime import datetime
 import os
 
+# Define device type and data type
+device_type = "cuda"
+data_type = torch.bfloat16
+
 # Get script directory for relative paths
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 print("Loading model...")
 pipe = FluxKontextPipeline.from_pretrained(
-    "black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=torch.bfloat16
+    "black-forest-labs/FLUX.1-Kontext-dev", torch_dtype=data_type
 )
-pipe.to("cuda")
-print("Model loaded!")
+pipe.to(device_type)
+
+print("Appliying optimizations...")
+pipe.enable_model_cpu_offload()
+pipe.enable_sequential_cpu_offload()
+pipe.enable_attention_slicing()
+pipe.enable_vae_slicing()
+print("Model loaded... ")
 
 # Load input image using absolute path
 input_image_path = os.path.join(script_dir, "default.png")
@@ -22,7 +32,11 @@ print(f"Input image loaded: {input_image_path}")
 image = pipe(
     image=input_image,
     prompt="Add a beach background with palm trees and a bright sunny sky.",
+    width=768,
+    height=1024,
     guidance_scale=2.5,
+    num_inference_steps=20,
+    generator=torch.Generator(device=device_type).manual_seed(42),
 ).images[0]
 
 # Save with program name and timestamp
